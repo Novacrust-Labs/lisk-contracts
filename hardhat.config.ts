96d4960ddb6c33dd02bb57cacb564143d70b34d9
goodnesskay <gtkbrain@gmail.com>
2025-12-17 02:13:52 +0100

    configure lisk sepolia

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,6 @@
 import hardhatToolboxViemPlugin from "@nomicfoundation/hardhat-toolbox-viem";
 import { configVariable, defineConfig } from "hardhat/config";
+import "dotenv/config";
 
 export default defineConfig({
   plugins: [hardhatToolboxViemPlugin],
@@ -33,6 +34,12 @@ export default defineConfig({
       chainType: "l1",
       url: configVariable("SEPOLIA_RPC_URL"),
       accounts: [configVariable("SEPOLIA_PRIVATE_KEY")],
+    },
+    "lisk-sepolia": {
+      type: "http",
+      url: "https://rpc.sepolia-api.lisk.com",
+      accounts: [process.env.WALLET_KEY as string],
+      gasPrice: 1000000000,
     },
   },
 });
